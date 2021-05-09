44605fb950281e746295ceb3a72ccaf05df78d66
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-09 19:15:44 -0300

    finished

--- 06-Product-Of-Array/product-of-array.js
+++ 06-Product-Of-Array/product-of-array.js
@@ -7,4 +7,22 @@
  * @returns {Number} - deberia retornar un number.
  */
 
-function productOfArray(arr) {}
+function productOfArray(arr) {
+    var resultado = 0;
+    for (let i = 0; i < arr.length; i++) {
+     for (let j = i + 1; j < arr.length; j++) {
+         resultado = resultado * arr[i] * arr[j];
+     }   
+    }
+    return resultado;
+}
+
+//function productOfArray(arr) {
+    //if (arr.length === 0) return 1;
+    //return arr[0] * productOfArray(arr.slice(1));
+  //}
+  
+  
+  // * El método slice() devuelve una copia de una parte del array dentro de un nuevo array
+  // * empezando por inicio hasta fin (fin no incluido). El array original no se modificará.
+  // * arr.slice([inicio [, fin]])
