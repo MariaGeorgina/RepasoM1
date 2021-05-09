c6014a8673b6973e7291636568ec668c409b6315
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-09 17:54:20 -0300

    finished

--- 01-Same-Naive/same-naive.js
+++ 01-Same-Naive/same-naive.js
@@ -9,4 +9,17 @@
  * @returns {boolean} - deberia retornar true o false.
  */
 
-function same(arr1, arr2) {}
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i ++) {
+    let correctIndex = arr2.indexOf (arr1[i] ** 2);
+    if (correctIndex === -1) {
+        return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+}
+return true;
+}
