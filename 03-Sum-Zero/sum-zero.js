413bca12689a68083a591ee5bf6acba89d7b04dc
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-09 17:59:15 -0300

    finished

--- 03-Sum-Zero/sum-zero.js
+++ 03-Sum-Zero/sum-zero.js
@@ -8,4 +8,10 @@
  * @returns {Array || undefined} - deberia retornar un array o undefined.
  */
 
-function sumZero(arr) {}
+function sumZero(arr) {
+    for (let i = 0; i < arr.length; i++)
+    for (let j = i + 1; j < arr.length; j++)
+      if (arr[i] + arr[j] === 0) // si encuentra el par de nros que den '0'
+        return [arr[i], arr[j]]; // retorna el par de nros 
+};
+
