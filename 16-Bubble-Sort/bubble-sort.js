f54a24b867e6f7e37e31d218aac54b8831fa3883
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-19 16:01:03 -0300

    finished

--- 16-Bubble-Sort/bubble-sort.js
+++ 16-Bubble-Sort/bubble-sort.js
@@ -1,3 +1,16 @@
 function bubbleSort(arr) {
   // construye un algoritmo de busqueda bubble sort
+  let swap = true;
+  while (swap) {
+      swap = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr [i + 1]) {
+        let aux = arr [i]
+        arr [i] = arr [i + 1];
+        arr [i + 1] = aux;
+        swap = true;
+      }
+    }
+  } 
+    return arr;
 }
