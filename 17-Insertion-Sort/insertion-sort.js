ba429071525e3e8115813d9b3b5b255beedb59fc
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-20 08:54:45 -0300

    finished

--- 17-Insertion-Sort/insertion-sort.js
+++ 17-Insertion-Sort/insertion-sort.js
@@ -1,3 +1,14 @@
 function insertionSort(arr) {
   // construye un algoritmo de busqueda bubble sort
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let aux = arr [i];
+    while (j >= 0 && arr[j] > aux) {
+      arr [j + 1] = arr [j];
+      j--;
+    }
+    arr [j + 1] = aux;
+  }
+  return arr;
 }
+
