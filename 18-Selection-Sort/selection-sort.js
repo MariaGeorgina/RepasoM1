a7cb5c303569aa05d8ce1c10b888f341059fbc74
MariaGeorgina <mgbaldassin@gmail.com>
2021-05-20 09:02:16 -0300

    finished

--- 18-Selection-Sort/selection-sort.js
+++ 18-Selection-Sort/selection-sort.js
@@ -1,3 +1,18 @@
 function selectionSort(arr) {
   // construye un algoritmo de busqueda bubble sort
+  for (var i = 0; i < arr.length; i++) {
+    var min = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      var aux = arr[i];
+      arr [i] = arr [min];
+      arr[min] = aux;
+    }
+  }
+  return arr;
 }
+
